dde29fe507909c616297d676eb6da51b6e391562
Heeransh Siddhu <heeransh.siddhu@bighaat.com>
2025-03-24 18:54:37 +0530

    Concatenating message with jsonRequestMessage in chatReply controller

--- src/controllers/chatControllers.ts
+++ src/controllers/chatControllers.ts
@@ -1,19 +1,23 @@
-import { Request, Response } from 'express'
-import { StatusCodes } from 'http-status-codes'
-import chatWithGPT from '../services/chatWithGPT'
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import chatWithGPT from "../services/chatWithGPT";
 
 export const chatReply = async (req: Request, res: Response): Promise<void> => {
-  const { message } = req.body
+  const { message } = req.body;
 
+  const jsonRequestMessage =
+    "Convert the above tasks into JSON. Array of objects. Each object with the following fields: id, title, isCompleted. isComplete set to false by default.";
+
+  const updatedPrompt = `${message} ${jsonRequestMessage}`;
   try {
-    const response = await chatWithGPT(message)
+    const response = await chatWithGPT(updatedPrompt);
 
     res.status(StatusCodes.OK).json({
       message: response,
-    })
+    });
   } catch (error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      message: 'Failed to process request',
-    })
+      message: "Failed to process request",
+    });
   }
-}
+};
