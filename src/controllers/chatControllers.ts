fec9c923bc2c5947770e64232793dd5d7a2a7141
Heeransh Siddhu <heeransh.siddhu@bighaat.com>
2025-03-28 10:19:21 +0530

    Updated chatReply controller

--- src/controllers/chatControllers.ts
+++ src/controllers/chatControllers.ts
@@ -2,6 +2,34 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import chatWithGPT from "../services/chatWithGPT";
 
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: "Check Emails",
+//     isCompleted: false,
+//   },
+//   {
+//     id: 2,
+//     title: "Complete Work Task",
+//     isCompleted: false,
+//   },
+//   {
+//     id: 3,
+//     title: "Exercise",
+//     isCompleted: false,
+//   },
+//   {
+//     id: 4,
+//     title: "Grocery Shopping",
+//     isCompleted: false,
+//   },
+//   {
+//     id: 5,
+//     title: "Read for 20 Minutes",
+//     isCompleted: false,
+//   },
+// ];
+
 export const chatReply = async (req: Request, res: Response): Promise<void> => {
   const { message } = req.body;
 
@@ -11,9 +39,8 @@ export const chatReply = async (req: Request, res: Response): Promise<void> => {
   const updatedPrompt = `${message} ${jsonRequestMessage}`;
   try {
     const response = await chatWithGPT(updatedPrompt);
-
     res.status(StatusCodes.OK).json({
-      message: response,
+      message: JSON.parse(response),
     });
   } catch (error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
